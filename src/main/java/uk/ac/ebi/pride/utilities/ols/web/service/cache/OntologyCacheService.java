7d32066a97a548bd8572c3eeb0a58531413bbc2f
avinash <inuganti@ebi.ac.uk>
2019-04-01 11:11:24 +0100

    add caching for ontology lookup service

---
+++ src/main/java/uk/ac/ebi/pride/utilities/ols/web/service/cache/OntologyCacheService.java
@@ -0,0 +1,52 @@
+package uk.ac.ebi.pride.utilities.ols.web.service.cache;
+
+import org.springframework.stereotype.Service;
+import uk.ac.ebi.pride.utilities.ols.web.service.client.OLSClient;
+import uk.ac.ebi.pride.utilities.ols.web.service.config.OLSWsConfigProd;
+import uk.ac.ebi.pride.utilities.ols.web.service.model.Identifier;
+import uk.ac.ebi.pride.utilities.ols.web.service.model.Term;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class OntologyCacheService {
+
+    private static OLSClient olsClient = new OLSClient(new OLSWsConfigProd());
+
+    private Set<String> ontologyTermSet;
+    private AtomicInteger size=new AtomicInteger(0);
+    private final int maxCapacity=100;
+
+    OntologyCacheService(){
+        System.out.println("######## New Ontology Cache Object Initialized ##########");
+        //creating a concurrent hash set implementation for java 8
+        ontologyTermSet = ConcurrentHashMap.newKeySet(maxCapacity);
+    }
+
+    public boolean isTermExisting(String attributeAccession, String ontologyAccession){
+        StringBuilder ontologyKey = new StringBuilder(attributeAccession+ontologyAccession);
+        if(ontologyTermSet.contains(ontologyKey.toString())){
+            System.out.println("Ontology Cache Current Size:"+size.get());
+            return true;
+        }else{
+            Term term =  olsClient.getTermById(new Identifier(attributeAccession, Identifier.IdentifierType.OBO), ontologyAccession);
+            if(term!=null){
+                //check if cache size is exceeding
+                if(size.get()<maxCapacity) {
+                    ontologyTermSet.add(ontologyKey.toString());
+                    size.incrementAndGet();
+                    System.out.println("Ontology Cache Size Incremented:"+size.get());
+                }
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+
+
+}
